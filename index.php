09a0de9dbee6da37c16fb1fdfa844e8914a13e9c
Nguyen Do <dothnguyen@gmail.com>
2017-01-10 14:57:17 +1000

    table structure modify

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-<!--Index page-->
+<?php
+// check for login
+require_once "db.php";
+require_once "user_functions.php";
+require_once "other_functions.php";
+
+session_start();
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
+    <title>Boral Checklists Management</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-3 col-sm-6 ">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="js/jquery-1.12.3.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
