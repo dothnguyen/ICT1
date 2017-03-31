c29fa1bf69f7dda47917dce363e683a8150d52e7
Nguyen Do <dothnguyen@gmail.com>
2017-03-31 21:57:57 +1000

    upload file finish, save checklist to db

--- rep_daily_chklist.php
+++ rep_daily_chklist.php
@@ -21,6 +21,9 @@ $login_user = $_SESSION['user_info'];
 
 $has_allocation = true;
 
+$site_alloc_id = 0;
+$site_name = "";
+
 // handle form submission
 if (isset($_REQUEST['submit'])) {
 
@@ -56,11 +59,36 @@ if (isset($_REQUEST['submit'])) {
     }
 
     // save data
-    $checklist = $_POST['checklist'];
+    $checklist1 = isset($_POST['checklist1']) ? $_POST['checklist1'] : 0;
+    $checklist2 = isset($_POST['checklist2']) ? $_POST['checklist2'] : 0;
+    $checklist3 = isset($_POST['checklist3']) ? $_POST['checklist3'] : 0;
+    $checklist4 = isset($_POST['checklist4']) ? $_POST['checklist4'] : 0;
+    $checklist5 = isset($_POST['checklist5']) ? $_POST['checklist5'] : 0;
+    $checklist6 = isset($_POST['checklist6']) ? $_POST['checklist6'] : 0;
+    $checklist7 = isset($_POST['checklist7']) ? $_POST['checklist7'] : 0;
+    $checklist8 = isset($_POST['checklist8']) ? $_POST['checklist8'] : 0;
+    $checklist9 = isset($_POST['checklist9']) ? $_POST['checklist9'] : 0;
+
     $comment = test_input($_POST['comment']);
+    $site_alloc_id = test_input($_POST['site_alloc_id']);
+
+    $conn = db_connect();
 
     // save data to daily checklist
-    $sql = "INSERT INTO daily(d_created_date, d_comments, d_checklist1, d_checklist2, d_checklist3, d_checklist4, d_checklist5, d_checklist6, d_checklist7, d_checklist8, d_checklist9, site_alloc_id)";
+    $sql = "INSERT INTO daily(d_created_date, d_comments, d_checklist1, d_checklist2, d_checklist3, d_checklist4, d_checklist5, d_checklist6, d_checklist7, d_checklist8, d_checklist9, site_alloc_id)
+                  VALUES(NOW(), '$comment', $checklist1, $checklist2, $checklist3, $checklist4, $checklist5, $checklist6, $checklist7, $checklist8, $checklist9, $site_alloc_id);";
+
+    // insert
+    $conn->query($sql);
+    $last_id = $conn->insert_id;
+
+    // insert uploaded files
+    foreach ($saved_files as $file_path) {
+        $sql = "INSERT INTO upload(chklist_id, chklist_type, file_path) VALUES($last_id, 1, '$file_path');";
+        $conn->query($sql);
+    }
+
+    mysqli_close($conn);
 
 } else {
 
@@ -77,8 +105,12 @@ if (isset($_REQUEST['submit'])) {
         if ($user_allocation == null) {
             $has_allocation = false;
         } else {
-
+            $site_name = $user_allocation['site_name'];
+            $site_alloc_id = $user_allocation['site_alloc_id'];
         }
+
+        mysqli_close($conn);
+
     } else {
 
     }
@@ -89,7 +121,7 @@ if (isset($_REQUEST['submit'])) {
 /**
  */
 function get_user_allocation($conn, $user_id) {
-    $sql = "SELECT rep.*, site.site_name FROM representative_allocated rep, site WHERE user_id=$user_id AND site_rep_active_status = 1 AND rep.site_id = site.site_id";
+    $sql = "SELECT rep.*, site.site_name FROM representative_allocated rep, site WHERE user_id=$user_id AND site_rep_active_status = 1 AND rep.site_id = site.site_id;";
 
     $ret = $conn->query($sql);
 
@@ -137,7 +169,7 @@ function get_user_allocation($conn, $user_id) {
         </div>
         <div  class="row">
             <div class="col-md-2"></div>
-            <div class="col-md-8"><span class="title">For site: <strong><?php echo $user_allocation['site_name']; ?></strong></span></div>
+            <div class="col-md-8"><span class="title">For site: <strong><?php echo $site_name; ?></strong></span></div>
         </div>
         <div class="row checklist-title">
             <div class="col-md-2"></div>
@@ -150,31 +182,31 @@ function get_user_allocation($conn, $user_id) {
             <div class="col-md-8">
                 <div class="row control-wrapper">
                     <div class="col-md-4">
-                        <input type="checkbox" name="checklist" id="check1"> <label for="check1">Check 1</label>
+                        <input type="checkbox" name="checklist1" value="1" id="check1"> <label for="check1">Check 1</label>
                     </div>
                     <div class="col-md-4">
-                        <input type="checkbox" name="checklist" id="check2"> <label for="check2">Check 2</label>
+                        <input type="checkbox" name="checklist2" value="1" id="check2"> <label for="check2">Check 2</label>
                     </div>
                     <div class="col-md-4">
-                        <input type="checkbox" name="checklist" id="check3"> <label for="check3">Check 3</label>
+                        <input type="checkbox" name="checklist3" value="1" id="check3"> <label for="check3">Check 3</label>
                     </div>
                     <div class="col-md-4">
-                        <input type="checkbox" name="checklist" id="check4"> <label for="check4">Check 4</label>
+                        <input type="checkbox" name="checklist4" value="1" id="check4"> <label for="check4">Check 4</label>
                     </div>
                     <div class="col-md-4">
-                        <input type="checkbox" name="checklist" id="check5"> <label for="check5">Check 5</label>
+                        <input type="checkbox" name="checklist5" value="1" id="check5"> <label for="check5">Check 5</label>
                     </div>
                     <div class="col-md-4">
-                        <input type="checkbox" name="checklist" id="check6"> <label for="check6">Check 6</label>
+                        <input type="checkbox" name="checklist6" value="1" id="check6"> <label for="check6">Check 6</label>
                     </div>
                     <div class="col-md-4">
-                        <input type="checkbox" name="checklist" id="check7"> <label for="check7">Check 7</label>
+                        <input type="checkbox" name="checklist7" value="1" id="check7"> <label for="check7">Check 7</label>
                     </div>
                     <div class="col-md-4">
-                        <input type="checkbox" name="checklist" id="check8"> <label for="check8">Check 8</label>
+                        <input type="checkbox" name="checklist8" value="1" id="check8"> <label for="check8">Check 8</label>
                     </div>
                     <div class="col-md-4">
-                        <input type="checkbox" name="checklist" id="check9"> <label for="check9">Check 9</label>
+                        <input type="checkbox" name="checklist9" value="1" id="check9"> <label for="check9">Check 9</label>
                     </div>
                 </div>
             </div>
@@ -223,6 +255,7 @@ function get_user_allocation($conn, $user_id) {
             </div>
         </div>
     </div>
+        <input type="hidden" name="site_alloc_id" value="<?php echo $site_alloc_id?>">
     </form>
     <?php } ?>
 </section>
