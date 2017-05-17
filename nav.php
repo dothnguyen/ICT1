8280d0d0dae546820d608c667ad83590e86fe8f6
Nguyen Do <dothnguyen@gmail.com>
2017-05-17 21:55:41 +1000

    add bootstrap-select lib

--- nav.php
+++ nav.php
@@ -37,26 +37,26 @@ $page_name = basename($_SERVER['PHP_SELF']);
         <div class = "collapse navbar-collapse" id = "example-navbar-collapse">
             <ul class = "nav navbar-nav">
                 <?php if ($page_name == 'manager_home.php') {?>
-                    <li class = "active"><a href = "manager_home.php">Home</a></li>
+                    <li class = "active"><a href = "#">Home</a></li>
                 <?php } else {?>
                     <li><a href = "manager_home.php">Home</a></li>
                 <?php }?>
 
                 <?php if ($page_name == 'manager_report.php') {?>
-                    <li class = "active"><a href = "manager_report.php">Reports</a></li>
+                    <li class = "active"><a href = "#">Reports</a></li>
                 <?php } else {?>
                     <li><a href = "manager_report.php">Reports</a></li>
                 <?php }?>
 
                 <?php if ($page_name == 'site_manage.php' || $page_name == 'site_modify.php') {?>
-                    <li class = "active"><a href = "site_manage.php">Site Management</a></li>
+                    <li class = "active"><a href = "#">Site Management</a></li>
                 <?php } else {?>
                     <li><a href = "site_manage.php">Site Manage</a></li>
                 <?php }?>
 				
 				
                 <?php if ($page_name == 'user_manage.php' || $page_name == 'user_modify.php') {?>
-                    <li class = "active"><a href = "user_manage.php">User Management</a></li>
+                    <li class = "active"><a href = "#">User Management</a></li>
                 <?php } else {?>
                     <li><a href = "user_manage.php">User Manage</a></li>
                 <?php }?>
